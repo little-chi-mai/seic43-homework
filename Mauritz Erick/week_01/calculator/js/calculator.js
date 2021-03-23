ac75057a2ce2dab8946007c715d695e64ab02f23
Mauritz Erick <mauritzerick@Mauritzs-MacBook-Air.local>
2021-03-24 10:25:09 +1100

    calculator

--- Mauritz Erick/week_01/calculator/js/calculator.js
+++ Mauritz Erick/week_01/calculator/js/calculator.js
@@ -1,41 +1,43 @@
 //Part1
 console.log("Part 1 Calculator")
 function squareNumber(number){
-  squared = Math.pow(number, 2);
-  console.log("The result of squaring the number " + number + " is " + squared);
+  let squared = Math.pow(number, 2);
+  console.log("\nThe result of squaring the number " + number + " is " + squared);
+  return squared;
 }
 squareNumber(5);
 
 function halfNumber(num){
-  divide = num / 2;
-  console.log("\nHalf of " + num + " is " + divide);
+  let divide = num / 2;
+  console.log("Half of " + num + " is " + divide);
+  return divide;
 }
 halfNumber(5);
 
 function percentOf(a,b){
-  percentIs = (a/b*100);
+  let percentIs = (a/b*100);
   console.log(a + " is " + percentIs + "% of " + b);
+  return percentIs;
 }
 percentOf(2,8);
-
 function areaOfCircle(radius){
-  area = (Math.PI * radius * radius);
-  area = area.toFixed(2); //Bonus part
-  console.log("\nThe area for a circle with radius " + radius + " is " + area )
+  var area = (Math.PI * radius * radius);
+  var area = area.toFixed(2); //Bonus part
+  console.log("The area for a circle with radius " + radius + " is " + area )
+  return area;
 }
 areaOfCircle(2);
 
 
 //Part2
 console.log("\n\n\n\nPart 2 Calculator")
-function part2(a){
-  half = a / 2;
-  sq = Math.pow(half, 2);
-  area = 3.14 * sq * sq;
-  percent = (sq/area*100);
+function part2(inside){
+  let half = halfNumber(inside);
+  let sq = squareNumber(half);
+  let area2 = areaOfCircle(sq);
+  let percent = percentOf(sq,area2);
 
-  console.log("original value: " + a + "\n#1 Half: " + half + "\n#2 Square: "+  sq + "\n#3 Area: "+ area);
+  console.log("original value: " + inside + "\n#1 Half: " + half + "\n#2 Square: "+  sq + "\n#3 Area: "+ area2);
   console.log("\nthe percentage is " + percent + " %")
 }
-
-part2(50);
+part2(500);
