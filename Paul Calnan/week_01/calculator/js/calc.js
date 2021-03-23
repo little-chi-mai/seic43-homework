752f2ad301645e09630d2e8f16cf4a7154fefa2a
Paul Calnan <pcalnan71@gmail.com>
2021-03-24 08:54:54 +1100

    Part1 and 2 functions homework

--- Paul Calnan/week_01/calculator/js/calc.js
+++ Paul Calnan/week_01/calculator/js/calc.js
@@ -3,6 +3,7 @@
 const squareNumber = function(number){
   const result = (number * number);
   console.log(`The result of squaring the number ${ number } is ${ result }`)
+  return result;
 };
 
 squareNumber(5);
@@ -11,6 +12,7 @@ squareNumber(5);
 const halfNumber = function(number){
   const result = number / 2;
   console.log(`The result of halving the number ${ number } is ${ result }`)
+  return result;
 };
 
 halfNumber(5);
@@ -19,6 +21,7 @@ halfNumber(5);
 const percentOf = function(number1, number2){
   const result = number1 / number2 * 100;
   console.log(`${ number1 } is ${ result }% of ${ number2 }`)
+  return result;
 };
 
 percentOf(2, 4);
@@ -28,21 +31,15 @@ const areaOfCircle = function(radius){
   const area = Math.PI * radius**2;
   const rounded = area.toFixed(2);
   console.log(`The Area of a circle with radius ${ radius } is ${ area } or ${ rounded } rounded`)
+  return area;
+  return rounded;
   };
 
 areaOfCircle(2);
 
-//Part2
-
-//Another failed attempt
-// const part2 = function(aNumber)
-//   const result1 = halfNumber(aNumber);
-//     console.log(result1);
-//     const result2 = squareNumber(result1);
-//       console.log(result2);
-
+//Part 2
 const part2 = function(aNumber){
-  const result1 = halfNumber(aNumber)// Code breaks here, not passing result1 onto next??(Scope? Return?)
+  const result1 = halfNumber(aNumber);
   const result2 = squareNumber(result1);
   const result3 = areaOfCircle(result2);
   const result4 = percentOf(result3, result2);
