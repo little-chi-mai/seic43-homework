e14a438d0c8e55f52a95a49c6f45138db2e24e14
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-03-26 10:01:06 +1100

    Added Luhn Test to credit card validation

--- Fei (David) Ye/week_01/day04/Credit Card Validation/js/Credit Card Validation.js
+++ Fei (David) Ye/week_01/day04/Credit Card Validation/js/Credit Card Validation.js
@@ -74,11 +74,54 @@ const validateCreditCard = (cardNumber) => {
     isSumOfAllDigitsGreaterThan16 = false;
   }
 
+  let lunhTest = true;
+
+  let oddDigits = [];
+
+  let evenDigits = [];
+
+ cardNumberArrToNumberTypeReverse = cardNumberArrToNumberType.reverse();
+
+  for (let i = 0; i < cardNumberArrToNumberTypeReverse.length; i++) {
+    if (i % 2 !== 0) {
+      evenDigits.push(cardNumberArrToNumberType[i]);
+    } else {
+      oddDigits.push(cardNumberArrToNumberType[i]);
+    }
+  }
+
+  const sumOfAllOddDigits = oddDigits.reduce((a, b) => a + b, 0);
+
+  const evenDigitsEachMultiplytwo = evenDigits.map((num)=>num*2);
+
+  const evenDigitsEachMultiplytwoStr = evenDigitsEachMultiplytwo.map((num)=>num+'');
+
+  for (let i = 0; i < evenDigitsEachMultiplytwoStr.length; i++) {
+    if (evenDigitsEachMultiplytwoStr[i].length===2) {
+      let splitNums=evenDigitsEachMultiplytwoStr[i].split('');
+      for (let j = 0; j < splitNums.length; j++) {
+        splitNums[j]=splitNums[j]-0;
+      }
+      evenDigitsEachMultiplytwoStr[i] = splitNums[0]+splitNums[1];
+    }
+    
+  }
+
+
+  const sumOfAllEvenDigitsAfterFormula = evenDigitsEachMultiplytwoStr.map((num)=>num-0).reduce((a, b) => a + b, 0);
+
+  if ((sumOfAllOddDigits+sumOfAllEvenDigitsAfterFormula)%10!==0) {
+    lunhTest=false;
+  }
+
+
+
   const checkList = [
     is16Digits,
     isMoreThanTwoDifferentDigits,
     isLastDigitEven,
     isSumOfAllDigitsGreaterThan16,
+    lunhTest
   ];
 
   const errorList = [
@@ -86,15 +129,15 @@ const validateCreditCard = (cardNumber) => {
     "no_more_than_3_different_digits",
     "last_digit_not_even",
     "sum_of_all_digits_less_than_16",
+    "luhn_test_failed"
   ];
 
   let errorsHappened = [];
 
   for (let i = 0; i < checkList.length; i++) {
-      if (!checkList[i]) {
-          errorsHappened+=(errorList[i]+', ');
-      }
-      
+    if (!checkList[i]) {
+      errorsHappened += errorList[i] + ", ";
+    }
   }
 
   let isValid = true;
@@ -103,7 +146,8 @@ const validateCreditCard = (cardNumber) => {
     is16Digits &&
     isMoreThanTwoDifferentDigits &&
     isLastDigitEven &&
-    isSumOfAllDigitsGreaterThan16
+    isSumOfAllDigitsGreaterThan16 &&
+    lunhTest
   ) {
     isValid = true;
   } else {
@@ -111,18 +155,17 @@ const validateCreditCard = (cardNumber) => {
   }
 
   let returnCardInfo = {
-      valid:isValid,
-      number: cardNumber
+    valid: isValid,
+    number: cardNumber,
+  };
+
+  if (isValid == false) {
+    returnCardInfo.error = errorsHappened;
   }
 
-  if (isValid==false) {
-      returnCardInfo.error=errorsHappened;
-  }
-
-return returnCardInfo;
+  return returnCardInfo;
 };
 
-console.log(validateCreditCard(`5999-5999-8888-0000`));
 console.log(validateCreditCard(`6666-6666-6666-1666`));
 console.log(validateCreditCard(`a923-3211-9c01-1112`));
 console.log(validateCreditCard(`4444-4444-4444-4444`));
@@ -130,10 +173,14 @@ console.log(validateCreditCard(`1111-1111-1111-1110`));
 console.log(validateCreditCard(`6666-6666-6666-6661`));
 console.log(validateCreditCard(`1111-1111-1111-1104`));
 
-// *Double Bonus*: Make your credit card scheme even more advanced! What are the rules, and what are some numbers that pass or fail? Ideas: check expiration date! Check out the Luhn Algorithm for inspiration.
+console.log(validateCreditCard(`5217-2918-7104-8701`));
 
+console.log(validateCreditCard(`4328-6213-7594-1622`));
+console.log(validateCreditCard(`5234-5263-9626-5606`));
+
+
+
+// *Double Bonus*: Make your credit card scheme even more advanced! What are the rules, and what are some numbers that pass or fail? Ideas: check expiration date! Check out the Luhn Algorithm for inspiration.
 
 //Luhn Algorithm
 //https://www.dcode.fr/luhn-algorithm
-
-
