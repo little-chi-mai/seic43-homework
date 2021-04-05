525b69fd4047fc10d594ccf884bfefde74263b5f
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-04-05 15:09:38 +1000

    homework of David Ye

---
+++ Fei (David) Ye/week_02/atm/js/atmfail.js
@@ -0,0 +1,83 @@
+// code failed
+
+// $(document).ready(function () {
+
+//     $('#checking-deposit').on('click',showDepositAmount);
+
+//     let total=0;
+    
+
+//     function showDepositAmount() {
+//         let depositAmount= $('#checking-amount').val()-0;
+//         total+=depositAmount;
+//         $('#checking-balance').text(total);
+//     }
+
+//     $('#checking-deposit').on('click',showDepositAmount);
+    
+
+// //     const currentBalance=(accountType)=>{
+// //         return $(`#${accountType}-balance`).text().split('')[1]-0;
+// //     }
+
+// //     const inputAmount=(accountType)=>{
+// //         return $(`#${accountType}-amount`).val()-0;
+// //     }
+
+
+
+// //     const deposit=(accountType,amount)=>{
+// //         const depositBtn = $(`#${accountType}-deposit`);
+// //         const increaseBalance=()=>{
+// //             currentBalance(accountType)=currentBalance(accountType)+=amount;
+// //         }
+// //         depositBtn.on('click',increaseBalance);
+// //     }
+
+    
+
+// //   let $checkingBalance = $("#checking-balance").text().split('')[1]-0;
+
+// //   let $savingsBalance = $("#savings-balance").text().split('')[1]-0;
+
+// //   const $checkingDepositBtn = $("#checking-deposit");
+
+// //   const $savingsDepositBtn = $("#savings-deposit");
+
+
+// //   let result=($(`#checking-balance`).text().split('')[1]-0);
+
+// //   let input=($(`#checking-amount`).val()-0);
+
+// //   $savingsDepositBtn.on('click',function () {
+// //     //   console.log('hey');
+// //     const accountType = 'checking';
+// //     // $(`#${accountType}-balance`).text(`$${$(`#${accountType}-balance`).text().split('')[1]-0+100}`);
+// //     // $("#checking-balance")
+// //     let a =$("#checking-balance").text();
+// //     // result+=input;
+// //     console.log(input);
+// //     console.log(result);
+// //     $("#checking-balance").text(result);
+
+// //     return result+input;
+// //   })
+
+// //   const $checkingWithdrawBtn = $("#checking-withdraw");
+
+// //   const $savingsWithdrawBtn = $("#savings-withdraw");
+
+// //   const $checkingAmountBtn = $("#checking-amount");
+
+// //   const $savingsAmountBtn = $("#savings-amount");
+
+// //   $checkingBalance+=1;
+
+// //   console.log($("#checking-balance").text());
+
+// //   console.log('hey');
+// //   console.log(inputAmount('checking'));
+
+
+
+// });
