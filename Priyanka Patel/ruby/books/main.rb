fbb60c0216173d8ac0cd9fd4cd7667cbc228a143
Priyanka Patel <priyanka.syd@gmail.com>
2021-04-14 23:13:20 +1000

    Homework_books

---
+++ Priyanka Patel/ruby/books/main.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+require 'httparty'
+
+#  GET https://www.googleapis.com/books/v1/volumes?q={title}
+
+get '/' do
+  erb :home
+end
+
+get '/hello' do
+  @name = params["name"]
+  "hello "+ @name
+end
+
+get '/search' do
+#   #get book title
+@title = params["booktitle"]
+#   #fetch book info
+url ="https://www.googleapis.com/books/v1/volumes?q=title:#{@title}"
+@info = HTTParty.get url
+@author =@info["items"][0]["volumeInfo"]["authors"]
+@cover = @info["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+  erb :book
+end
