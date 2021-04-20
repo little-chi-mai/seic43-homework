5dc9d57db37ad9458fd1de9b260a771d827df8ed
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-04-20 23:14:07 +1000

    updated

--- Fei (David) Ye/week_05/zodiac--system/config/routes.rb
+++ Fei (David) Ye/week_05/zodiac--system/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
   get '/zodiacs/index' => 'zodiacs#index'
   get '/zodiacs/new' => 'zodiacs#new', :as => :new_zodiac
-  post '/zodiacs/index' => 'zodiacs#create'
+  post '/zodiacs/' => 'zodiacs#create'
   get '/zodiacs/:id' => 'zodiacs#show', :as => :zodiac
   get '/zodiacs/:id/edit' => 'zodiacs#edit', :as => :edit_zodiac
   post '/zodiacs/:id' => 'zodiacs#update'
