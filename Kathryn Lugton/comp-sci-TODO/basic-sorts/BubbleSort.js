669e9b1be809d1857b2a1a27221a22587d427989
mac rent <macrent@macs-MacBook-Air-2.local>
2021-05-09 13:57:23 +1000

    Week 7 - Weekend Homework - React Tic Tac Toe

--- Kathryn Lugton/comp-sci-TODO/basic-sorts/BubbleSort.js
+++ Kathryn Lugton/comp-sci-TODO/basic-sorts/BubbleSort.js
@@ -1,20 +1,21 @@
 function bubbleSort(array) {
-    // Make some sort of loop that keeps happening if there were any swaps
-    // that happened this turn
 
-    // reset swapHappened to false each time so we can detect if a swap
-    // happened in this specific iteration.
+  let needToIterate = true;  //assume the worst
 
-    // Make another loop (inside the first one) to go through one 
-    // round of swapping from the start of the list to the end
-    
-    // Inside inner loop:
-    // compare each pair of elements near each other
-    // swap them if the bigger one was at a lower index.
+  let end = array.length - 1;
 
-    // Make sure to keep track of whether a swap happened!
+  while(needToIterate === true){
+    needToIterate = false; //we haven't had to move anything yet
 
-    // After both loops have exited, remember to return the array
+    for(let i = 0; i < end; i++){
+
+      if(array[i] > array[i+1]){
+    [ array[i], array[i+1] ] = [ array[i+1], array[i] ]
+        needToIterate = true;
+      }
+    }
+    end--;
+  }
     return array;
 }
 
