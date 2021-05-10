5759dbd9332c3451cfd479e6237adf1f51c5a2ae
Mauritz Erick <mauritzerick@192-168-1-101.tpgi.com.au>
2021-05-11 02:54:30 +1000

    week 8 day 1

---
+++ Mauritz Erick/week_08/day_1/crypto-tracker/src/App.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import './App.css';
+import Coin from './Coin';
+
+
+function App() {
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=aud&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+
+    }).catch(error => alert('There is an error'));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    )
+
+
+  return (
+    <div className='coin-app'>
+      <div className="coin-search">
+        <h1 className="coin-text">To the Moon 🚀</h1>
+          <form>
+            <input type="text" placeholder="Search"
+             className="coin-input" onChange={handleChange}/>
+          </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return <Coin 
+        key={coin.id} 
+        name={coin.name} 
+        image={coin.image}
+        symbol={coin.symbol}
+        marketcap={coin.market_cap}
+        price={coin.current_price}
+        priceChange={coin.price_change_percentage_24h}
+        volume={coin.total_volume}
+        />;
+        
+      })}
+    </div>
+  );
+}
+
+export default App;
